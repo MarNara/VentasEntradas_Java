3af7195578f9bb4c11927be70179f8fbb1ab0728
RafaMango <rafaelabascunan2005@gmail.com>
2025-08-31 16:44:07 -0400

    Update Operador.java

--- Ventas/src/entradas/Operador.java
+++ Ventas/src/entradas/Operador.java
@@ -1,5 +1,17 @@
 package entradas;
 
 public class Operador {
+    private String nombre;
 
+    public Operador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public void crearEvento(SistemaEntradas sistema, String nombreEvento, String lugar, String fecha, double costoBase) {
+        sistema.registrarEvento(nombreEvento, lugar, fecha, costoBase);
+    }
 }
+
